3747f446b69900c0ca404b53ec001a8aa027c612
RunesSI <rubenssi825@gmail.com>
2022-10-18 19:14:59 -0300

    Add axios

--- src/views/home.js
+++ src/views/home.js
@@ -1,10 +1,35 @@
 import React from "react"
+import axios from "axios"
+
+
 
 class Home extends React.Component {
+
+  state = {
+    nomeUsuario: ""
+  }
+
+  componentDidMount() {
+    // recuparar usuario logado no localStorage
+    const usuarioLogadoString = localStorage.getItem('-usuario_logado')
+    // transformar objeto recebido do localHistorage em json
+    const usuarioLogado = JSON.parse(usuarioLogadoString)
+
+    // fazer uma requisição para obter o nome do usuario loga para exiber na navbar
+    // obter usuario por ai
+    axios.get(`http://localhost:8080/api/usuarios/${usuarioLogado}/usuario`)
+      .then(response => {
+        this.setState({nomeUsuario: response.data})
+      }).catch(error => {
+        console.log(error.response)
+      })
+  }
+
   render() {
     return (
       <div className="jumbotron">
-        <h1 className="display-3">Bem vindo!</h1>
+        <h1 className="display-3">Bem vindo! <span>{this.setState.nomeUsuario}</span></h1>
+
         <p className="lead">Esse é seu sistema de Gerenciamneto de medicações.</p>
         <hr className="my-4" />
         <p>E essa é sua área administrativa, utilize um dos menus ou botões abaixo para navegar pelo sistema.</p>
